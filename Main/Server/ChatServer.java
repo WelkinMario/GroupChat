3fa637f5b5044a1a9993898b46207181957654e8
yuhos <welkin0820@gmail.com>
2022-08-18 18:09:18 -0400

    Server: add synchronized methods to manipulate list variables in ChatServer

--- Main/Server/ChatServer.java
+++ Main/Server/ChatServer.java
@@ -8,17 +8,27 @@ import java.util.List;
 
 public class ChatServer {
     public static List<Socket> socketList = new ArrayList<>();
+    public static List<ServerClient> clientList = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(5555);
         System.out.println("Chat room created");
         while (true) {
             Socket socket = server.accept();
-            socketList.add(socket);
+            addClient(new ServerClient(socket));
             System.out.println("Connected from " + socket.getInetAddress()
                                 + ":" + socket.getPort());
             new Thread(new ServerThread(socket)).start();
         }
     }
 
+    public static synchronized void addClient(ServerClient client) {
+        clientList.add(client);
+        socketList.add(client.socket);
+    }
+
+    public static synchronized void rmvClient(ServerClient client) {
+        clientList.remove(client);
+        socketList.remove(client.socket);
+    }
 }
